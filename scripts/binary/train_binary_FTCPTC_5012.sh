8e5b82f8ad7d006b6a2f7f7a19949e3a274e2531
bodongwang <bodongwang@tencent.com>
2026-07-12 18:57:38 +0800

    edit

--- scripts/binary/train_binary_FTCPTC_5012.sh
+++ scripts/binary/train_binary_FTCPTC_5012.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 设置训练参数
-Train_DATASET="LNM_4795"
+Train_DATASET="FTCPTC_4795"
 CUDA_VISIBLE_DEVICES="1"
 METHOD="dino_unet"
 TRAIN_IMAGE_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/images/"
 TRAIN_MASK_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/masks/"
-TRAIN_LABEL_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/Malignant_labels_4795.json"  # 添加分类标签路径
-EPOCH=50
+TRAIN_LABEL_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/Malignant_labels_5012.json"  # 添加分类标签路径
+EPOCH=40
 LR=1e-4
 BATCH_SIZE=12
-CHECKPOINT_DIR="/mnt/wangbd8/workspace/ThyroidAgent/dino_unet_multitask/checkpoints/LNM_4795"
+CHECKPOINT_DIR="/mnt/wangbd8/workspace/ThyroidAgent/dino_unet_multitask/checkpoints/FTCPTC_4795"
 CHECKPOINT_INTERVAL=5
 EVAL_INTERVAL=5
 DATASET_NAME="train_multitask_${Train_DATASET}"
@@ -18,22 +18,22 @@ DATASET_NAME="train_multitask_${Train_DATASET}"
 # 使用数组配置多个测试数据集
 # 测试数据集名称数组
 TEST_DATASET_NAMES=(
-    "LNM_CN01"
+    "FTCPTC_FangDai"
 )
 
 # 测试图像路径数组
 TEST_IMAGE_PATHS=(
-    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/images/"
+    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/FangDai_Thyroid_Ultrasound_Images_cropped/"
 )
 
 # 测试掩码路径数组
 TEST_MASK_PATHS=(
-    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/masks/"
+    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/FangDai_Thyroid_Ultrasound_Images_cropped_predictions/"
 )
 
 # 测试分类标签路径数组
 TEST_LABEL_PATHS=(
-    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/LymphUs_test_labels.json"
+    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/FangDai_Thyroid_Ultrasound_Images_cropped/FangDai_test_labels.json"
 )
 
 # 确保数组长度一致
@@ -75,7 +75,7 @@ python train_multitask_binary_gamtl.py \
     --test_mask_paths $MASK_PATHS_ARGS \
     --test_label_paths $LABEL_PATHS_ARGS \
     --test_dataset_names $DATASET_NAMES_ARGS \
-    --target_key "LNM_CN01" \
+    --target_key "FTCPTC" \
     --epoch $EPOCH \
     --lr $LR \
     --batch_size $BATCH_SIZE \
--- scripts/binary/train_binary_LNMCN01_5012.sh
+++ scripts/binary/train_binary_LNMCN01_5012.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 设置训练参数
-Train_DATASET="FTCPTC_4795"
+Train_DATASET="LNM_4795"
 CUDA_VISIBLE_DEVICES="1"
 METHOD="dino_unet"
 TRAIN_IMAGE_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/images/"
 TRAIN_MASK_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/masks/"
-TRAIN_LABEL_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/Malignant_labels_4795.json"  # 添加分类标签路径
-EPOCH=40
+TRAIN_LABEL_PATH="/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/Malignant_labels_5012.json"  # 添加分类标签路径
+EPOCH=50
 LR=1e-4
 BATCH_SIZE=12
-CHECKPOINT_DIR="/mnt/wangbd8/workspace/ThyroidAgent/dino_unet_multitask/checkpoints/FTCPTC_4795"
+CHECKPOINT_DIR="/mnt/wangbd8/workspace/ThyroidAgent/dino_unet_multitask/checkpoints/LNM_4795"
 CHECKPOINT_INTERVAL=5
 EVAL_INTERVAL=5
 DATASET_NAME="train_multitask_${Train_DATASET}"
@@ -18,22 +18,22 @@ DATASET_NAME="train_multitask_${Train_DATASET}"
 # 使用数组配置多个测试数据集
 # 测试数据集名称数组
 TEST_DATASET_NAMES=(
-    "FTCPTC_FangDai"
+    "LNM_CN01"
 )
 
 # 测试图像路径数组
 TEST_IMAGE_PATHS=(
-    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/FangDai_Thyroid_Ultrasound_Images_cropped/"
+    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/images/"
 )
 
 # 测试掩码路径数组
 TEST_MASK_PATHS=(
-    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/FangDai_Thyroid_Ultrasound_Images_cropped_predictions/"
+    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/masks/"
 )
 
 # 测试分类标签路径数组
 TEST_LABEL_PATHS=(
-    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/FangDai_Thyroid_Ultrasound_Images_cropped/FangDai_test_labels.json"
+    "/mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/Malignant_images/LymphUs_test_labels.json"
 )
 
 # 确保数组长度一致
@@ -75,7 +75,7 @@ python train_multitask_binary_gamtl.py \
     --test_mask_paths $MASK_PATHS_ARGS \
     --test_label_paths $LABEL_PATHS_ARGS \
     --test_dataset_names $DATASET_NAMES_ARGS \
-    --target_key "FTCPTC" \
+    --target_key "LNM_CN01" \
     --epoch $EPOCH \
     --lr $LR \
     --batch_size $BATCH_SIZE \
